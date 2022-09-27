c94d556ab8f1410d85c24743533a2537e684a3d5
Jaewon <woin2ee@gmail.com>
2022-09-27 15:00:44 +0900

    Pull to refresh 구현

--- PullToRefreshExample/Scenes/MainViewController.swift
+++ PullToRefreshExample/Scenes/MainViewController.swift
@@ -13,18 +13,25 @@ final class MainViewController: UIViewController {
         didSet {
             postTableView.dataSource = self
             postTableView.delegate = self
+            postTableView.refreshControl = .init()
+            postTableView.refreshControl?.addTarget(
+                self,
+                action: #selector(self.refreshPostTableView),
+                for: .valueChanged
+            )
         }
     }
     
     private var posts: [Post] = []
+    private var refreshCount = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.updatePosts()
     }
     
-    private func updatePosts() {
-        guard let url = URL.init(string: "https://jsonplaceholder.typicode.com/posts")
+    private func updatePosts(completion: (() -> Void)? = nil) {
+        guard let url = URL.init(string: "https://jsonplaceholder.typicode.com/posts/\(self.refreshCount)")
         else { return }
         
         var request: URLRequest = .init(url: url)
@@ -34,23 +41,39 @@ final class MainViewController: UIViewController {
         URLSession.shared.dataTask(with: request) { data, urlResponse, error in
             if let error = error {
                 debugPrint(error.localizedDescription)
+                completion?()
                 return
             }
             
             guard let data = data else {
+                completion?()
                 return
             }
             
-            guard let posts = try? JSONDecoder.init().decode(Array<Post>.self, from: data)
-            else { return }
+            guard let post = try? JSONDecoder.init().decode(Post.self, from: data)
+            else {
+                completion?()
+                return
+            }
             
-            self.posts = posts
+            self.posts.append(post)
             
             DispatchQueue.main.async {
                 self.postTableView.reloadData()
             }
+            
+            completion?()
         }
         .resume()
+    }
+    
+    @objc private func refreshPostTableView() {
+        self.refreshCount += 1
+        self.updatePosts {
+            DispatchQueue.main.async {
+                self.postTableView.refreshControl?.endRefreshing()
+            }
+        }
     }
 }
 
